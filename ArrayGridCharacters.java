c6c126299e296fcbb2d6413e08fdaa8501423961
hwwilson1204 <57260833+hwwilson1204@users.noreply.github.com>
2019-11-01 08:56:11 -0500

    Add files via upload

---
+++ ArrayGridCharacters.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+
+public class ArrayGridCharacters {
+
+	public static Scanner console = new Scanner(System.in);
+	public static Random r = new Random();
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char[] spaces = {'1', '|', '2', '|', '3', '4', '|', '5', '|', '6', '7', '|', '8', '|', '9'};
+		aiInput(spaces);
+		updateGrid(spaces);
+	}
+
+
+
+
+
+
+
+	public static void aiInput(char[] spaces) {
+		int randInt=r.nextInt(9)+49;
+		char randChar = (char) randInt;
+		for (char ch : spaces) {
+			if (randChar != ch) {
+				
+			}
+		}
+	}
+
+	public static void hInput(char[] spaces) {
+		
+	}
+
+	public static void updateGrid(char[] spaces) {
+		
+	}
+}
+
+
+
