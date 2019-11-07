5980662988cdcb86d4238823e158a183bcc0f54f
hwwilson1204 <57260833+hwwilson1204@users.noreply.github.com>
2019-11-07 08:32:49 -0600

    Update ArrayGridCharacters.java

--- ArrayGridCharacters.java
+++ ArrayGridCharacters.java
@@ -1,15 +1,16 @@
 import java.io.*;
 import java.util.*;
 
-
-public class ArrayGridCharacters {
+public class arrayCharGrid {
 
 	public static Scanner console = new Scanner(System.in);
 	public static Random r = new Random();
-	
+	public static final String hgLine = "-----------"; //horizontal lines that intersect vertical lines to create 3x3 grid	
+
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		char[] spaces = {'1', '|', '2', '|', '3', '4', '|', '5', '|', '6', '7', '|', '8', '|', '9'};
+		char[][] spaces = {{'1', '|', '2', '|', '3'}, {'4', '|', '5', '|', '6'}, {'7', '|', '8', '|', '9'}};
 		aiInput(spaces);
 		updateGrid(spaces);
 	}
@@ -20,37 +21,33 @@ public class ArrayGridCharacters {
 
 
 
-	public static void aiInput(char[] spaces) {
+	public static void aiInput(char[][] spaces) {
 		int randInt=r.nextInt(9)+49;
 		char randChar = (char) randInt;
-		for (char ch : spaces) {
-			if (randChar != ch) {
-				
-			}
-		}
+
 	}
 
-	public static void hInput(char[] spaces) {
-		
+
+
+	public static void hInput(char[][] spaces) {
+
 	}
 
-	public static void updateGrid(char[] spaces) {
-			char[] row1= Arrays.copyOfRange(spaces, 0, 5); //top 3 squares
-			char[] row2= Arrays.copyOfRange(spaces, 5, 10);//middle 3 squares
-			char[] row3= Arrays.copyOfRange(spaces, 10, 15);//bottom 3 squares
-			for (int n=0; n<5; n++) {
-				System.out.print(" " +row1[n]);
-			}
-			System.out.println("\n"+hgLine);
-			for (int n=0; n<5; n++) {
-				System.out.print(" "+row2[n]);
-			}
-			System.out.println("\n"+hgLine);
-			for (int n=0; n<5; n++) {
-				System.out.print(" "+row3[n]);
-			}
-			// fencepost algorithm row 1 --> then hgLine and next row
+	public static void updateGrid(char[][] spaces) {
+		for(int l=0; l<5; l++) {
+			System.out.print(" "+spaces[0][l]);
 		}
+		System.out.println();
+		for (int n=1; n<3; n++) {
+			System.out.println(hgLine);
+			for (int m=0; m<5; m++) {
+				System.out.print(" "+spaces[n][m]);
+			}
+			System.out.println();
+		}
+	
+	}
+
 }
 
 
